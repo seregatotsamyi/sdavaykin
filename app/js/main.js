7254cf059be0291416100edb3a73772796007b29
seregatotsamyi <iserg.mos.ls@gmail.com>
2023-10-18 17:07:11 +0500

    advantages

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,13 @@ $(function () {
     body.toggleClass('_fixed')
   })
 
+  if (width < 577) {
+    $('.js-advantages-slider').slick({
+      autoplay: true,
+      appendArrows: '.slider__arrows',
+      prevArrow: '<div class="slider-arrow slider-arrow_prev">      <img class="slider-arrow__img" src="images/icon/slider-right.svg" alt="slider-right.svg" width="30"        height="30">    </div>',
+      nextArrow: ' <div class="slider-arrow slider-arrow_next">      <img class="slider-arrow__img" src="images/icon/slider-right.svg" alt="slider-right.svg" width="30"        height="30">    </div>',
+    })
+  }
+
 });

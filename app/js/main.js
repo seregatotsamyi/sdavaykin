a7e4d8dab5f50342ff7eb2364790dfa2134663bd
seregatotsamyi <iserg.mos.ls@gmail.com>
2023-10-18 11:00:29 +0500

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,18 @@
 window.onload = function () {
   "use strict";
+
 };
 
 
-$(function(){
+$(function () {
   let width = $(window).width();
   let body = $('.body');
+
+  let menu = $('.header__nav-wrap')
+
+  $(document).on('click', '.js-toggle-menu', function () {
+    menu.toggleClass('_active')
+    body.toggleClass('_fixed')
+  })
+
 });
